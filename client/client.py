923bde00a836c27f84605ced194687f7b2c444d2
KadZumi77 <yazykova.2004@mail.ru>
2024-04-06 00:44:40 +0300

    Creat game window

--- client/client.py
+++ client/client.py
@@ -1,18 +1,25 @@
 import socket
+import pygame
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
-sock.connect(('localhost',10000))
+#подключаемся к серверу
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+sock.connect(('localhost', 10000))
+
+#создание окна игры
+pygame.init()
+screen=pygame.display.set_mode((800,700))
+pygame.display.set_caption(' Agar.io')
 
 while True:
-    #считываем команду
+    # считываем команду
 
-    #отправляем команду на сервер
+    # отправляем команду на сервер
     sock.send('Хочу идти вправо'.encode())
 
-    #получаем от сервера новое состояние игрового поля
-    data=sock.recv(1024)
-    data=data.decode()
+    # получаем от сервера новое состояние игрового поля
+    data = sock.recv(1024)
+    data = data.decode()
 
-    #рисуем новое состояние игрового поля
+    # рисуем новое состояние игрового поля
     print(data)
